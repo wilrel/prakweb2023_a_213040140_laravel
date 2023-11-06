b40f324f1d80af89705ab8cd858b2ffb74e88ee6
wilrel <wildannasrulloh321@gmail.com>
2023-11-06 15:50:12 +0700

    Update Video 11

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,7 @@ Route::get('/about', function () {
 });
 
 
+
 Route::get('/blog', [PostController::class, 'index']);
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
@@ -48,8 +49,7 @@ Route::get('/categories', function () {
 Route::get('/categories/{category:slug}', function (Category $category) {
     return view('posts', [
         'title' => "Post by Category : $category->name",
-        'posts' => $category->posts,
-        'category' => $category->name
+        'posts' => $category->posts->load('category', 'author')
     ]);
 });
 
