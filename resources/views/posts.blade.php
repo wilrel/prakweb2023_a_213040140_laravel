ea10d7fa3e67e8048b421fa94696719b875409f2
wilrel <wildannasrulloh321@gmail.com>
2023-11-16 04:01:55 +0700

    Update Video 13 Pagination

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,6 +6,9 @@
 <div class="row justify-content-center mb-3">
   <div class="col-md-6">
     <form action="/posts">
+      @if (request('category'))
+      <input type="hidden" name="category" value="{{ request('category') }}">
+      @endif
       <div class="input-group mb-3">
         <input type="text" class="form-control" placeholder="Search.." name="search" value="{{ request('search') }}">
         <button class="btn btn-danger" type="submit">Search</button>
@@ -23,7 +26,7 @@
     <h3 class="card-title"><a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h3>
     <p>
       <small class="text-muted">
-        By. <a href="/authors/{{ $posts[0]->author->username }}" class="text-decoration-none">{{ $posts[0]->author->name }}</a> in <a href="/categories/{{ $posts[0]->category->slug }}" class="text-decoration-none">{{ $posts[0]->category->name }}</a> {{ $posts[0]->created_at->diffForHumans() }}
+        By. <a href="/posts?author={{ $posts[0]->author->username }}" class="text-decoration-none">{{ $posts[0]->author->name }}</a> in <a href="/posts?category={{ $posts[0]->category->slug }}" class="text-decoration-none">{{ $posts[0]->category->name }}</a> {{ $posts[0]->created_at->diffForHumans() }}
     </p>
     </small>
     <p class="card-text">{{ $posts[0]->excerpt }}</p>
@@ -39,13 +42,13 @@
 
     <div class="col-md-4 mb-3">
       <div class="card">
-        <div class="position-absolute bg-dark px-3 py-2" style="background-color: rgba(0,255,0,0.7)"><a href="/categories/{{ $posts[0]->category->slug }}" class="text-decoration-none text-white">{{ $post->category->name }}</a></div>
-        <img class="card-img-top" src="https://source.unsplash.com/500x400?{{ $post->category->name }}" alt="{{ $post->category->name }}">
+        <div class="position-absolute bg-dark px-3 py-2" style="background-color: rgba(0,255,0,0.7)"><a href="/posts?category={{ $post->category->slug }}" class="text-decoration-none text-white">{{ $post->category->name }}</a></div>
+        <img class=" card-img-top" src="https://source.unsplash.com/500x400?{{ $post->category->name }}" alt="{{ $post->category->name }}">
         <div class="card-body">
           <h5 class="card-title">{{ $post->title }}</h5>
           <p>
             <small class="text-muted">
-              By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> {{ $post->created_at->diffForHumans() }}
+              By. <a href="/posts?author={{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> {{ $post->created_at->diffForHumans() }}
           </p>
           <p class="card-text">{{ $post->excerpt }}</p>
           <a href="/posts/{{ $post->slug }}" class="btn btn-primary">Read More</a>
@@ -62,6 +65,8 @@
 <p class="text-center fs-4">No Post Found.</p>
 @endif
 
-
+<div class="d-flex justify-content-end">
+  {{ $posts->links() }}
+</div>
 
 @endsection
