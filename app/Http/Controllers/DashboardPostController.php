fb6c5d07d18fc34a770185ac5d7575c8ce3bf200
wilrel <wildannasrulloh321@gmail.com>
2023-11-27 23:02:50 +0700

    Video ke 19 Update

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -6,6 +6,7 @@ use App\Models\Post;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use \Cviebrock\EloquentSluggable\Services\SlugService;
+use Illuminate\Support\Str;
 
 class DashboardPostController extends Controller
 {
@@ -15,7 +16,6 @@ class DashboardPostController extends Controller
     public function index()
     {
         return view('dashboard.posts.index', [
-
             'posts' => Post::where('user_id', auth()->user()->id)->get()
         ]);
     }
@@ -35,7 +35,19 @@ class DashboardPostController extends Controller
      */
     public function store(Request $request)
     {
-        return $request;
+        $validatedData = $request->validate([
+            'title' => 'required|max:255',
+            'slug' => 'required|unique:posts',
+            'category_id' => 'required',
+            'body' => 'required'
+        ]);
+
+        $validatedData['user_id'] = auth()->user()->id;
+        $validatedData['excerpt'] = Str::limit(strip_tags($request->body), 200);
+
+        Post::create($validatedData);
+
+        return redirect('/dashboard/posts')->with('success', 'New post has been added!');
     }
 
     /**
@@ -74,7 +86,6 @@ class DashboardPostController extends Controller
 
     public function checkSlug(Request $request)
     {
-
         $slug = SlugService::createSlug(Post::class, 'slug', $request->title);
         return response()->json(['slug' => $slug]);
     }
